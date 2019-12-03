51ebb00f57aab07d9e52dc510633860e00ec333a
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-12-03 12:51:04 +0100

    gradle implementatnion added

---
+++ app/src/main/java/pl/piotrdawidziuk/quotesmvvmtutorial/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.piotrdawidziuk.quotesmvvmtutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
