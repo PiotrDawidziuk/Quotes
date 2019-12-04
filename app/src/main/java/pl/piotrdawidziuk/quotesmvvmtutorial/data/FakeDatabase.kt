97e1bd19fafa64aff2d4bdc73d19d05087ad50aa
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-12-04 14:24:51 +0100

    fake database

---
+++ app/src/main/java/pl/piotrdawidziuk/quotesmvvmtutorial/data/FakeDatabase.kt
@@ -0,0 +1,16 @@
+package pl.piotrdawidziuk.quotesmvvmtutorial.data
+
+class FakeDatabase private constructor(){
+
+    companion object {
+        @Volatile private var instance: FakeDatabase? = null
+
+        var quoteDao = FakeQuoteDao()
+            private set 
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: FakeDatabase().also { instance = it }
+            }
+    }
+}
